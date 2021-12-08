f961db9296ead2b1ececfa90f4475f1fcab10746
Tolibjon11 <tolibjonodilqoziyev@gmail.com>
2021-12-08 11:29:02 +0500

    Tolibjon's portfolio

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const header = document.querySelector('.header');
 
-const header = document.querySelector('.header .container');
+let form = document.getElementById("form");
+let nameInput = document.getElementById('name');
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
 
-document.addEventListener('scroll', () => {
+function Form() {
+    // console.log("Submit Clicked!")
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("telephone").value = "";
+    document.getElementById("textarea").value = "";
+}
+
+document.addEventListener('scroll', () => { 
     var scroll_position = window.scrollY;
     if (scroll_position > 100) {
         header.style.backgroundColor = "#29323c";
